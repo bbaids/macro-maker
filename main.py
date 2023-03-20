bc5d7aa38d22af4a227b0e983f9751c90d5c7ea6
Brian Baids <brian.baids20@gmail.com>
2023-03-19 21:27:06 -0500

    Added processing after stopping recording and play function

--- main.py
+++ main.py
@@ -10,37 +10,50 @@ class Recording:
     name = None
 
     def record(self):
+        if not self.is_empty:
+            self.actions = []
+            play_button.forget()
+        
         self.is_recording = True
         self.is_empty = False
         print("Now Recording")
         mouse.on_click(self.record_click)
-
-    def play(self):
-        print(self.actions)
 
     def record_click(self):
         if self.is_recording:
             self.actions.append({
                 "action": "left",
                 "position": mouse.get_position(),
-                "time": time.time(),
-                "delay": lambda : 0 if len(self.actions) == 0 else 
+                "time": time.time()
             })
 
-        print(len(self.actions))
-
     def stop_recording(self):
-        if self.is_recording:
-            self.actions.pop()
         self.is_recording = False
         mouse.unhook_all()
-        
 
-    def read_recording(self):
+        #Process the recorded actions
+        recording_length = len(self.actions)
+        print(recording_length)
+        for i in range(0, recording_length):
+            if i == recording_length - 2:
+                self.actions.pop()
+                break
+            else:
+                print(i)
+                self.actions[i]['delay'] = self.actions[i+1]['time'] - self.actions[i]['time']
+
+        play_button.grid(column=3, row=0)
+
+    def play_recording(self):
         if self.is_recording:
             print("Please stop your recording first")
         else:
             print(self.actions)
+
+        for action in self.actions:
+            mouse.move(action['position'][0], action['position'][1])
+            mouse.click()
+            time.sleep(action['delay'])
 
 current_recording = Recording()
 
@@ -51,12 +64,11 @@ menu_frame = ttk.Frame(root)
 
 record_button = ttk.Button(menu_frame, text="Record", command=current_recording.record)
 stop_button = ttk.Button(menu_frame, text="Stop", command=current_recording.stop_recording)
-read_button = ttk.Button(menu_frame, text="Read", command=current_recording.read_recording)
+play_button = ttk.Button(menu_frame, text="Play", command=current_recording.play_recording)
 
 menu_frame.grid(column=0, row=0)
 record_button.grid(column=0, row=0)
 stop_button.grid(column=1, row=0)
-read_button.grid(column=2, row=0)
 
 root.mainloop()
 
